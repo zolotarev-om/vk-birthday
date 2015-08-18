ac02754dbe3db6bc1e6564f825d64f78c7b5a484
Oleg Zolotarev <zolotarev.om@gmail.com>
2015-08-18 21:02:47 +0300

    add dynamic caching middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,12 +1,12 @@
 <?php
 
-Route::get('/', ['as' => 'main', 'middleware' => 'auth', 'uses' => 'IndexController@index']);
+Route::get('/', ['as' => 'main', 'middleware' => ['auth', 'cache.dynamic'], 'uses' => 'IndexController@index']);
 
 Route::get('login', ['as' => 'login', 'uses' => 'AuthController@loginPage']);
 Route::get('login/{provider?}', ['as' => 'loginProvider', 'uses' => 'AuthController@preLogin']);
 Route::get('logout', ['as' => 'logout', 'middleware' => 'auth', 'uses' => 'AuthController@logout']);
 
-Route::get('setting', ['as' => 'setting', 'middleware' => 'auth', 'uses' => 'SettingController@index']);
+Route::get('setting', ['as' => 'setting', 'middleware' => ['auth', 'cache.dynamic'], 'uses' => 'SettingController@index']);
 Route::post('setting/add', 'SettingController@addMessage');
 Route::post('setting/del', 'SettingController@deleteMessage');
 Route::post('setting/token', 'SettingController@updateToken');
