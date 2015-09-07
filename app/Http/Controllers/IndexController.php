3c64782204d68d5b128f4e3ae28daac588d7f2ff
Oleg Zolotarev <zolotarev.om@gmail.com>
2015-09-07 21:49:30 +0300

    delete unused code

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -62,7 +62,6 @@ class IndexController extends Controller
     public function index()
     {
         Event::fire(new UserLoginToMainPage($this->bday));
-        //$this->processCongratulations();
 
         $data = [];
         $data['latest'] = $this->formatLatestGratters();
@@ -70,44 +69,6 @@ class IndexController extends Controller
 
         return view('index')->with($data);
     }
-
-//    /**
-//     *  Get friends for congratulations, if they have - get random message,
-//     *  send message to friend and add sended gratters
-//     */
-//    private function processCongratulations()
-//    {
-//        $bdayers = $this->bday->friendsForCongratulations();
-//
-//        if (!empty($bdayers)) {
-//            foreach ($bdayers as $friend) {
-//                $msg = $this->messageRep->getRandomMessage();
-//                $this->sendMessage($friend, current($msg)); //text message
-//                $this->gratterRep->addSendedGratters($friend['vk_id'], key($msg)); // key(id) message
-//            }
-//        }
-//    }
-
-//    /**
-//     * Select destination to send message and execute VK api to this
-//     *
-//     * @param $user array
-//     * @param $msg  string
-//     *
-//     * @return bool
-//     */
-//    private function sendMessage($user, $msg)
-//    {
-//        if ($user['can_post'] == 1) {
-//            $this->reqApi->sendToWall($user['vk_id'], $msg);
-//            return true;
-//        } elseif ($user['can_write_private_message'] == 1) {
-//            $this->reqApi->sendToPrivate($user['vk_id'], $msg);
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
 
     /**
      * Formatting last gratters to be displayed on the main page
